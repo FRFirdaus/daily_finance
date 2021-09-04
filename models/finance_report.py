a74f0eb2e3c193f4e06980ca81ef300d55446f92
REHAN LC <frayhands@gmail.com>
2021-09-04 21:00:03 +0700

    add button preview pdf on report

--- models/finance_report.py
+++ models/finance_report.py
@@ -1,31 +1,31 @@
 from odoo import fields, models, api, _
 
 class FinanceReport(models.Model):
-    _name = 'finance.report'
+	_name = 'finance.report'
 
-    partner_id = fields.Many2one('res.partner', required=True, default=lambda self: self.env.user.partner_id)
-    start_date = fields.Date()
-    end_date = fields.Date()
-    report_line_ids = fields.One2many('finance.report.line', 'finance_report_id')
-    sub_total = fields.Float(digits = (12,2), readonly=True)
-    total_balance = fields.Float(digits = (12,2), readonly=True)
+	partner_id = fields.Many2one('res.partner', required=True, default=lambda self: self.env.user.partner_id)
+	start_date = fields.Date()
+	end_date = fields.Date()
+	report_line_ids = fields.One2many('finance.report.line', 'finance_report_id')
+	sub_total = fields.Float(digits = (12,2), readonly=True)
+	total_balance = fields.Float(digits = (12,2), readonly=True)
 
-    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.user.company_id.currency_id)
+	currency_id = fields.Many2one('res.currency', default=lambda self: self.env.user.company_id.currency_id)
 
-    def _get_report_base_filename(self):
-        return self.display_name
+	def _get_report_base_filename(self):
+		return self.display_name
 
-    def name_get(self):
-        result = []
-        for rec in self:
-            result.append((rec.id, "[Report] %s | %s" % (
-                self.partner_id.name,
-                self.create_date
-            )))
+	def name_get(self):
+		result = []
+		for rec in self:
+			result.append((rec.id, "[Report] %s | %s" % (
+				self.partner_id.name,
+				self.create_date
+			)))
 
-        return result
-    
-    def generate_pdf_url(self):
+		return result
+	
+	def generate_pdf_url(self):
 		base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
 		report_ref = 'daily_finance.finance_pdf_report'
 		raport_pdf_name = "Report Finance %s" % (self.partner_id.name)
@@ -47,20 +47,20 @@ class FinanceReport(models.Model):
 		}
 
 class FinanceReportLine(models.Model):
-    _name = 'finance.report.line'
+	_name = 'finance.report.line'
 
-    finance_report_id = fields.Many2one('finance.report', ondelete="cascade")
-    type = fields.Selection([
-        ('income', 'Income'),
-        ('outcome', 'Outcome')
-    ])
+	finance_report_id = fields.Many2one('finance.report', ondelete="cascade")
+	type = fields.Selection([
+		('income', 'Income'),
+		('outcome', 'Outcome')
+	])
 
-    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.user.company_id.currency_id)
-    food = fields.Char()
-    salary = fields.Char()
-    entertain = fields.Char()
-    bill = fields.Char()
-    charity = fields.Char()
-    other = fields.Char()
+	currency_id = fields.Many2one('res.currency', default=lambda self: self.env.user.company_id.currency_id)
+	food = fields.Char()
+	salary = fields.Char()
+	entertain = fields.Char()
+	bill = fields.Char()
+	charity = fields.Char()
+	other = fields.Char()
 
-    total = fields.Float()
+	total = fields.Float()
